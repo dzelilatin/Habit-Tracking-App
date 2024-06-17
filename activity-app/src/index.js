2620796c549021bbf054203dfed1155435182709
Dželila <dzelila.tinjak@stu.ibu.edu.ba>
2024-06-18 00:09:30 +0200

    bug fixes

--- activity-app/src/index.js
+++ activity-app/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -34,40 +33,3 @@ createRoot(rootELement).render(
 );
 
 reportWebVitals();
-=======
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import RegisterPage from './components/RegisterPage';
-import LoginPage from './components/LoginPage';
-
-const rootELement = document.getElementById('root');
-
-createRoot(rootELement).render(
-  <React.StrictMode>
-    <Router>
-    <Routes>
-  <Route path="/register" element={<RegisterPage />} />
-  <Route path="/login" element={<LoginPage />} />
-  <Route
-    path="/"
-    element={
-      localStorage.getItem('token') ? (
-        <App />
-      ) : (
-        <Navigate to="/login" />
-      )
-    }
-  />
-</Routes>
-
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
->>>>>>> main
--- activity-app/tailwind.config.js
+++ activity-app/tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,15 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> main
 }
